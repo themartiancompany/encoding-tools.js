55e36fe9b7b0a3fcf5e3936fd134a6d95ffe1a97
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-06-19 01:41:05 +0000

    update fs-worker

--- fs-worker.webpack.config.cjs
+++ fs-worker.webpack.config.cjs
@@ -15,13 +15,27 @@ const
 const
   _output_file_name =
     `${_input_file_name}.js`;
+_output = {
+  path:
+    _output_dir,
+  filename:
+    _output_file_name
+};
 const
-  _output = {
-    path:
-      _output_dir,
-    filename:
-      _output_file_name
-  };
+  _node_fs_ignore =
+  { resourceRegExp:
+      /^node:fs$/ };
+const
+  _webpack =
+    require(
+     "webpack");
+const
+  _ignore_plugin =
+    _webpack.IgnorePlugin; 
+const
+  _node_fs_ignore_plugin =
+    new _ignore_plugin(
+          _node_fs_ignore);
 module.exports = {
   entry:
     _input_file_path,
@@ -45,5 +59,8 @@ module.exports = {
           __dirname,
           'node_modules/path/mod.js'),
     },
-  }
+  },
+  plugins: [
+    _node_fs_ignore_plugin,
+  ]
 };
